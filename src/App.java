aa722f5af90b6ee04846da68ec69729b0102d781
tuva9 <tuva9@fpt.edu.vn>
2024-10-05 08:41:37 +0700

    implement linked list

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 
+// import package
+import DataStructure.LinkedList;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        LinkedList list = new LinkedList();
+        list.add(10);
+        list.add(20);
+        list.add(50);
+        list.add(100);
+        list.add(100);
+        list.add(100);
+        list.add(100);
+        list.add(100);
+        list.add(100);
+        list.add(100);
+        list.add(100);
+        list.print();
     }
 }
--- src/DataStructure/Array.java
+++ src/DataStructure/Array.java
@@ -8,9 +8,20 @@ public class Array {
                 "BH01144,9,8.5,10,9,7.5,9,8,10",
                 "BH01244,7,8.5,8,9,7.5,7.5,8,10",
         };
-        for (String s : students) {
-            getAverage(s);
-        }
+        // for (String s : students) {
+        // getAverage(s);
+        // }
+
+        // Copy array
+        // int[] a = { 2, 4, 6, 8 };
+        // int[] b = new int[4];
+        // // b = a;
+        // // b[0] = 100;
+        // // System.out.println(a);
+        // // System.out.println(b);
+        // // System.out.println(Arrays.toString(a));
+        // System.arraycopy(a, 0, b, 0, a.length);
+        // System.out.println(Arrays.toString(b));
 
     }
 
