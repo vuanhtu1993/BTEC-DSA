504665d22fb5a1d18143ec36010d7091269d48e5
tuva9 <tuva9@fpt.edu.vn>
2025-01-10 08:28:00 +0700

    Buổi 2: OOP

--- src/App.java
+++ src/App.java
@@ -1,18 +1,52 @@
-
-import Assignment.Student;
-import Helper.readCSV;
-import java.util.List;
-
-// import package
-
 public class App {
     public static void main(String[] args) throws Exception {
-        String csvFile = "student.csv";
-        List<Student> students = readCSV.csvToArray(csvFile);
-        for (Student student : students) {
-            student.print();
-        }
-        System.out.println(students.size());
+        // Object oriented programming
+        // Class
+        // Object c1 kieu Car
+        Car myCar = new Car();
+        Car c2 = new Car("Toyota", "red", "Cross", 2023);
+        System.out.println(myCar);
+        myCar.brake();
+        // Bài tập trên lớp
+        // Sử dụng ngôn ngữ Java tạo ra một lớp sinh viên BTEC
+        // Có các thuộc tính:
+        // - Mã sinh viên
+        // - Tên sinh viên
+        // - Lớp
+        // - DOB
+        // - Province
+        // Có các phương thức
+        // - In ra màn hình thông tin của sinh viên "Mã sv + Tên sv + lớp"
+        // - In ra tuổi của sinh viên
+    }
 
+}
+
+class Car {
+    // Fields
+    String company;
+    String model;
+    int year;
+    String color;
+
+    // Constructor
+    public Car() {
+        this.company = "BMW";
+        this.color = "White";
+        this.model = "W6";
+        this.year = 2024;
+    }
+
+    public Car(String company, String color, String model,
+            int year) {
+        this.company = company;
+        this.color = color;
+        this.model = model;
+        this.year = year;
+    }
+
+    // Methods
+    public void brake() {
+        System.out.print("The car is braking");
     }
 }
