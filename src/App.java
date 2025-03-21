f1b306973d75dafc4297280a3ca94ebf7b6dcd8e
tuva9 <tuva9@fpt.edu.vn>
2025-03-21 10:15:21 +0700

    Merge sort

--- src/App.java
+++ src/App.java
@@ -1,21 +1,23 @@
-import Algorithm.MergeSort;
+import Algorithm.QuickSort;
 import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int n = 100000;
-        int[] testArray = new int[n];
-        for (int i = 0; i < testArray.length; i++) {
-            testArray[i] = (int) (Math.random() * 100000000);
-        }
-        long startTime = System.currentTimeMillis();
-        // int[] testArray = { 20, 50, 40, 80, 60, 70, 10, 90 };
+        // int n = 100000;
+        // int[] testArray = new int[n];
+        // for (int i = 0; i < testArray.length; i++) {
+        // testArray[i] = (int) (Math.random() * 100000000);
+        // }
+        // long startTime = System.currentTimeMillis();
+        // // int[] testArray = { 20, 50, 40, 80, 60, 70, 10, 90 };
 
-        MergeSort.mergeSort(testArray);
-        // bubbleSort.bubbleSort(testArray);
-        long estimatedTime = System.currentTimeMillis() - startTime;
-        System.out.println(Arrays.toString(testArray));
-        System.out.println(estimatedTime);
-
+        // MergeSort.mergeSort(testArray);
+        // // bubbleSort.bubbleSort(testArray);
+        // long estimatedTime = System.currentTimeMillis() - startTime;
+        // System.out.println(Arrays.toString(testArray));
+        // System.out.println(estimatedTime);
+        int[] testArr = { 10, 80, 30, 90, 60 };
+        QuickSort.quickSort(testArr, 0, testArr.length - 1);
+        System.out.println(Arrays.toString(testArr));
     }
 }
